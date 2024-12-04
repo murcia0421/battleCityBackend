a61291ea48cf9d047195dcaa6ea45ef5147a39fc
lalaro <100946295+lalaro@users.noreply.github.com>
2024-12-04 13:54:58 -0500

    Rename variables gameroom CHECK

--- src/main/java/com/battlecity/battle_city_backend/services/GameRoomService.java
+++ src/main/java/com/battlecity/battle_city_backend/services/GameRoomService.java
@@ -2,57 +2,55 @@ package com.battlecity.battle_city_backend.services;
 
 import com.battlecity.model.GameRoom;
 import com.battlecity.model.Player;
-import com.battlecity.model.Position;
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class GameRoomService {
+
     private Map<String, GameRoom> rooms;
 
-
     public GameRoomService() {
-
         rooms = new HashMap<>();
+        initializeRooms();  // initialize rooms at the start
     }
 
-    public void initialiceRoom(){
+    // Initialize rooms with predefined values
+    public void initializeRooms() {
         rooms.put("room1", new GameRoom("room1", 2));
         rooms.put("room2", new GameRoom("room2", 2));
         rooms.put("room3", new GameRoom("room3", 2));
         rooms.put("room4", new GameRoom("room4", 2));
     }
 
-    public boolean addPlayerToRoom(String _roomId, Player _player){
-        GameRoom gameRoom = rooms.get(_roomId);
-
-        if(gameRoom != null && gameRoom.canPlayer()){
-            gameRoom.addPlayer(_player);
+    // Adds player to the specified room
+    public boolean addPlayerToRoom(String roomId, Player player) {
+        GameRoom gameRoom = rooms.get(roomId);
+        if (gameRoom != null && gameRoom.canPlayer()) {
+            gameRoom.addPlayer(player);
             return true;
         }
         return false;
     }
 
-    public GameRoom getRoom(String _roomId){
-        return rooms.get(_roomId);
+    // Retrieves the game room by its ID
+    public GameRoom getRoom(String roomId) {
+        return rooms.get(roomId);
     }
 
-    public void removePlayerToRoom(String _roomId, Player _player){
-        GameRoom gameRoom = rooms.get(_roomId);
-
-        if(gameRoom != null ){
-            gameRoom.deletePlayer(_player.getId());
+    // Removes a player from the specified room
+    public void removePlayerFromRoom(String roomId, Player player) {
+        GameRoom gameRoom = rooms.get(roomId);
+        if (gameRoom != null) {
+            gameRoom.deletePlayer(player.getId());
         }
     }
 
-    public List<Player> getPlayersInRoom(String _roomId){
-        GameRoom gameRoom = rooms.get(_roomId);
-        return gameRoom != null ? gameRoom.getPlayers() : null ;
+    // Retrieves the list of players in the specified room
+    public List<Player> getPlayersInRoom(String roomId) {
+        GameRoom gameRoom = rooms.get(roomId);
+        return gameRoom != null ? gameRoom.getPlayers() : null;
     }
-
-
-
 }
