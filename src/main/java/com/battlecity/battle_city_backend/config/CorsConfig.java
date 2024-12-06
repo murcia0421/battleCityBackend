4960433c39e5b2e35eb6e1a65e416d3a9b6db61e
lalaro <100946295+lalaro@users.noreply.github.com>
2024-12-05 22:54:44 -0500

    Cambios para solucionar el merge

--- src/main/java/com/battlecity/battle_city_backend/config/CorsConfig.java
+++ src/main/java/com/battlecity/battle_city_backend/config/CorsConfig.java
@@ -1,6 +1,10 @@
+package com.battlecity.battle_city_backend.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import lombok.NonNull;
+
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
